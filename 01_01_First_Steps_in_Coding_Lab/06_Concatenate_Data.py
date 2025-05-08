88438d651330d68c3f039bf08b3034d838a6969b
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-05-08 20:17:22 +0300

    add 06_Concatenate_Data.py

---
+++ 01_01_First_Steps_in_Coding_Lab/06_Concatenate_Data.py
@@ -0,0 +1,8 @@
+first_name_value = input()
+last_name_value = input()
+age_value = int(input())
+town_value = input()
+
+description_text = f'You are {first_name_value} {last_name_value}, a {age_value}-years old person from {town_value}.'
+
+print(description_text)
