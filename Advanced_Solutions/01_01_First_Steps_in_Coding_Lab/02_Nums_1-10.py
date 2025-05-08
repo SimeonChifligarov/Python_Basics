3735485586864ec7ac5245794d783b7414e838ac
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-05-08 20:22:17 +0300

    add 02_Nums_1-10.py

---
+++ Advanced_Solutions/01_01_First_Steps_in_Coding_Lab/02_Nums_1-10.py
@@ -0,0 +1,15 @@
+def print_numbers_1_to_10() -> None:
+    """
+    Prints numbers from 1 to 10. Each number goes on a new line.
+
+    Args:
+        None
+    Returns:
+        None
+    """
+
+    [print(number) for number in range(1, 11)]
+
+
+if __name__ == '__main__':
+    print_numbers_1_to_10()
