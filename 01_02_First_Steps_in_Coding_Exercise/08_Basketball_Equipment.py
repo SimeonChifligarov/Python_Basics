8173b7bcd833b0009f940f0b5e154e72a240db96
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-05-08 20:19:58 +0300

    add 08_Basketball_Equipment.py

---
+++ 01_02_First_Steps_in_Coding_Exercise/08_Basketball_Equipment.py
@@ -0,0 +1,10 @@
+fee_value = int(input())
+
+shoes_cost = fee_value * 0.6
+outfit_cost = shoes_cost * 0.8
+ball_cost = outfit_cost * 0.25
+accessories_cost = ball_cost * 0.2
+
+final_expense = fee_value + shoes_cost + outfit_cost + ball_cost + accessories_cost
+
+print(final_expense)
