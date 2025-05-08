33f70847963d78cf2816ab7bddf96fff87e0bfe5
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-05-08 20:17:33 +0300

    add 07_Projects_Creation.py

---
+++ 01_01_First_Steps_in_Coding_Lab/07_Projects_Creation.py
@@ -0,0 +1,7 @@
+architect_value = input()
+projects_value = int(input())
+
+hours_needed = projects_value * 3
+result_message = f'The architect {architect_value} will need {hours_needed} hours to complete {projects_value} project/s.'
+
+print(result_message)
