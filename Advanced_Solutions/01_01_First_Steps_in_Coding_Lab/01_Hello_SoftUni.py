c6b151f557fafe50b4ff446eb7c0db0d03159d1b
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-05-08 20:22:08 +0300

    add 01_Hello_SoftUni.py

---
+++ Advanced_Solutions/01_01_First_Steps_in_Coding_Lab/01_Hello_SoftUni.py
@@ -0,0 +1,14 @@
+def print_greeting() -> None:
+    """
+    Prints a greeting message.
+
+    Args:
+        None
+    Returns:
+        None
+    """
+    print('Hello SoftUni')
+
+
+if __name__ == '__main__':
+    print_greeting()
