764a7b5260ae0d380ccff20304f6fbf25f11063e
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-05-10 11:07:04 +0300

    add 01_Day_of_Week.py

---
+++ 03_01_Conditional_Statements_Advanced_Lab/01_Day_of_Week.py
@@ -0,0 +1,20 @@
+day_input = int(input())
+
+if day_input == 1:
+    result = 'Monday'
+elif day_input == 2:
+    result = 'Tuesday'
+elif day_input == 3:
+    result = 'Wednesday'
+elif day_input == 4:
+    result = 'Thursday'
+elif day_input == 5:
+    result = 'Friday'
+elif day_input == 6:
+    result = 'Saturday'
+elif day_input == 7:
+    result = 'Sunday'
+else:
+    result = 'Error'
+
+print(result)
