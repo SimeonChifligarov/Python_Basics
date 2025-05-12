d35ef1e202d89b1e84ffba245f51218cf90602a0
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-05-12 23:46:14 +0300

    add 07_Moving_wT.py

---
+++ Advanced_Solutions/05_02_While_Loop_Exercise/07_Moving_wT.py
@@ -0,0 +1,38 @@
+def track_moving_boxes(width: int, length: int, height: int) -> None:
+    """
+    This function tracks how many boxes can be moved into an apartment
+    based on the available cubic meters. Each box is 1 cubic meter.
+
+    Args:
+        width: the width of the space
+        length: the length of the space
+        height: the height of the space
+
+    Returns:
+        Nothing. Prints if space is enough or how much is missing.
+    """
+    total_volume: int = width * length * height
+    used_volume: int = 0
+
+    while True:
+        command: str = input()
+
+        if command == 'Done':
+            remaining: int = total_volume - used_volume
+            print(f'{remaining} Cubic meters left.')
+            return
+
+        boxes: int = int(command)
+        used_volume += boxes
+
+        if used_volume > total_volume:
+            shortage: int = used_volume - total_volume
+            print(f'No more free space! You need {shortage} Cubic meters more.')
+            return
+
+
+if __name__ == '__main__':
+    width_input: int = int(input())
+    length_input: int = int(input())
+    height_input: int = int(input())
+    track_moving_boxes(width=width_input, length=length_input, height=height_input)
