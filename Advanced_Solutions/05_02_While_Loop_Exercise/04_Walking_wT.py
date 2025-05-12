637ab1003fa31aa66cfc0c42115ba1b74d7a11e9
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-05-12 23:45:02 +0300

    add 04_Walking_wT.py

---
+++ Advanced_Solutions/05_02_While_Loop_Exercise/04_Walking_wT.py
@@ -0,0 +1,35 @@
+def track_steps() -> None:
+    """
+    This function helps Gabi track her steps throughout the day.
+    If she reaches her step goal, it congratulates her.
+    If she goes home early, it checks if she has met the goal or not.
+
+    Returns:
+        Nothing. Prints the outcome directly.
+    """
+    step_goal: int = 10000
+    total_steps: int = 0
+
+    while True:
+        command: str = input()
+
+        if command == 'Going home':
+            steps_home: int = int(input())
+            total_steps += steps_home
+            if total_steps >= step_goal:
+                print('Goal reached! Good job!')
+                print(f'{total_steps - step_goal} steps over the goal!')
+            else:
+                print(f'{step_goal - total_steps} more steps to reach goal.')
+            return
+
+        steps_walked: int = int(command)
+        total_steps += steps_walked
+        if total_steps >= step_goal:
+            print('Goal reached! Good job!')
+            print(f'{total_steps - step_goal} steps over the goal!')
+            return
+
+
+if __name__ == '__main__':
+    track_steps()
