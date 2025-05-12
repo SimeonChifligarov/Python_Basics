f60c2f8966ab052f4b27825e78699defce8e9329
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-05-12 23:45:24 +0300

    add 05_Coins_advanced.py

---
+++ Advanced_Solutions/05_02_While_Loop_Exercise/05_Coins_advanced.py
@@ -0,0 +1,36 @@
+from decimal import Decimal, ROUND_HALF_UP
+
+
+def minimum_coins_to_return(change: float) -> None:
+    """
+    This function calculates the minimum number of coins needed
+    to return a given amount of change.
+
+    Args:
+        change: the amount of money to return
+
+    Returns:
+        Nothing. Prints the number of coins.
+    """
+    change_decimal: Decimal = Decimal(str(change))
+    change_cents: int = int((change_decimal * Decimal('100')).to_integral_value(rounding=ROUND_HALF_UP))
+    coin_count: int = 0
+    coin_values: list[int] = [200, 100, 50, 20, 10, 5, 2, 1]
+
+    index: int = 0
+
+    while True:
+        if change_cents == 0:
+            print(coin_count)
+            return
+
+        if change_cents >= coin_values[index]:
+            change_cents -= coin_values[index]
+            coin_count += 1
+        else:
+            index += 1
+
+
+if __name__ == '__main__':
+    change_input: float = float(input())
+    minimum_coins_to_return(change=change_input)
