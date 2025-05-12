0639ed8f32332985bbd1ac4114968d7c4bf19dc3
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-05-12 23:34:34 +0300

    add 01_Read_Text.py

---
+++ 05_01_While_Loop_Lab/01_Read_Text.py
@@ -0,0 +1,12 @@
+# word = input()
+# while word != 'Stop':
+#     print(word)
+#     word = input()
+
+
+while True:
+    current_word = input()
+
+    if current_word == 'Stop':
+        break
+    print(current_word)
